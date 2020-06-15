e68ebc62e312081655948d71ab9c3de6998036e6
rsnikola <rsnikola@yahoo.com>
2020-06-16 00:49:08 +0200

    Gotovo cetvrto pitanje

--- Skripta.js
+++ Skripta.js
@@ -186,123 +186,183 @@ function getPublisherWithMostDlc () {
 
 
 function getMostBangForBuckMed () {
+    var retVal = db.steam_aggregate.aggregate(
+        [
+            {
+                $project: {
+                    "name": "$name", 
+                    "price": "$spy_data.price", 
+                    "average_hours": "$spy_data.median_forever",
+                    "is_free": "$is_free"
+                }
+            }
+            ,
+            {
+                $match: {
+                    "is_free": "False", 
+                    "price": {$ne: 0}, 
+                    "average_hours": {$ne: 0}
+                }
+            }
+            ,
+            {
+                $project: {
+                    "name": "$name", 
+                    "price": "$price", 
+                    "average_hours": "$average_hours",
+                    "price_per_hour": {"$ifNull": [{$divide: [{"$ifNull": ["$average_hours", 1.0]}, {"$ifNull": ["$price", 1.0]}]}, 0.0]}
+                }
+            }
+            ,
+            {
+                $sort: {
+                    "price_per_hour": -1
+                }
+            }
+            ,
+            {
+                $limit: 10
+            }
+            ,
+            {
+                $project: {
+                    "_id": "$name"
+                }
+            }
+        ]
+    );
+            
+    retVal = retVal.map( function(retVal) { return retVal._id; });
+    
+    var temp = ["", "", "", "", "", "", "", "", "", ""];
+           
+    for (var i = 0; i < 10; ++i) {
+        temp[i] = retVal[i];
+    }
+    retVal = temp;
+    
+    return retVal;
+}
+
+
+function getMostBangForBuckAvg () {
+    retVal = db.steam_aggregate.aggregate(
+        [
+            {
+                $project: {
+                    "name": "$name", 
+                    "price": "$spy_data.price", 
+                    "average_hours": "$spy_data.average_forever",
+                    "is_free": "$is_free"
+                }
+            }
+            ,
+            {
+                $match: {
+                    "is_free": "False", 
+                    "price": {$ne: 0}, 
+                    "average_hours": {$ne: 0}
+                }
+            }
+            ,
+            {
+                $project: {
+                    "name": "$name", 
+                    "price": "$price", 
+                    "average_hours": "$average_hours",
+                    "price_per_hour": {"$ifNull": [{$divide: [{"$ifNull": ["$average_hours", 1.0]}, {"$ifNull": ["$price", 1.0]}]}, 0.0]}
+                }
+            }
+            ,
+            {
+                $sort: {
+                    "price_per_hour": -1
+                }
+            }
+            ,
+            {
+                $limit: 10
+            }
+            ,
+            {
+                $project: {
+                    "_id": "$name"
+                }
+            }
+        ]
+    );
+            
+    retVal = retVal.map( function(retVal) { return retVal._id; });
+    
+    var temp = ["", "", "", "", "", "", "", "", "", ""];
+           
+    for (var i = 0; i < 10; ++i) {
+        temp[i] = retVal[i];
+    }
+    retVal = temp;
+    
+    return retVal;
+}
+
+
+function prosecnaOcenaBesplatnih () {
     var retVal = db.steam_aggregate.aggregate(
         [
             {
                 $project: {
-                    "name": "$name", 
-                    "price": "$spy_data.price", 
-                    "average_hours": "$spy_data.median_forever",
-                    "is_free": "$is_free"
+                    "is_free": "$is_free", 
+                    "rating": {$subtract: ["$spy_data.positive", "$spy_data.negative"]}
+                }
+            }
+            ,
+            {
+                $group: {
+                    "_id": "$is_free", 
+                    "rating": {$avg: "$rating"}
                 }
             }
             ,
             {
                 $match: {
-                    "is_free": "False", 
-                    "price": {$ne: 0}, 
-                    "average_hours": {$ne: 0}
-                }
-            }
-            ,
-            {
-                $project: {
-                    "name": "$name", 
-                    "price": "$price", 
-                    "average_hours": "$average_hours",
-                    "price_per_hour": {"$ifNull": [{$divide: [{"$ifNull": ["$average_hours", 1.0]}, {"$ifNull": ["$price", 1.0]}]}, 0.0]}
-                }
-            }
-            ,
-            {
-                $sort: {
-                    "price_per_hour": -1
-                }
-            }
-            ,
-            {
-                $limit: 10
-            }
-            ,
-            {
-                $project: {
-                    "_id": "$name"
+                    "_id": "True"
                 }
             }
         ]
     );
+    
             
-    retVal = retVal.map( function(retVal) { return retVal._id; });
-    
-    var temp = ["", "", "", "", "", "", "", "", "", ""];
-           
-    for (var i = 0; i < 10; ++i) {
-        temp[i] = retVal[i];
-    }
-    retVal = temp;
-    
-    return retVal;
+    return retVal.map( function(retVal) { return retVal.rating; })[0];
 }
 
 
-function getMostBangForBuckAvg () {
-    retVal = db.steam_aggregate.aggregate(
+function prosecnaOcenaPlatnih () {
+    var retVal = db.steam_aggregate.aggregate(
         [
             {
                 $project: {
-                    "name": "$name", 
-                    "price": "$spy_data.price", 
-                    "average_hours": "$spy_data.average_forever",
-                    "is_free": "$is_free"
+                    "is_free": "$is_free", 
+                    "rating": {$subtract: ["$spy_data.positive", "$spy_data.negative"]}
+                }
+            }
+            ,
+            {
+                $group: {
+                    "_id": "$is_free", 
+                    "rating": {$avg: "$rating"}
                 }
             }
             ,
             {
                 $match: {
-                    "is_free": "False", 
-                    "price": {$ne: 0}, 
-                    "average_hours": {$ne: 0}
-                }
-            }
-            ,
-            {
-                $project: {
-                    "name": "$name", 
-                    "price": "$price", 
-                    "average_hours": "$average_hours",
-                    "price_per_hour": {"$ifNull": [{$divide: [{"$ifNull": ["$average_hours", 1.0]}, {"$ifNull": ["$price", 1.0]}]}, 0.0]}
-                }
-            }
-            ,
-            {
-                $sort: {
-                    "price_per_hour": -1
-                }
-            }
-            ,
-            {
-                $limit: 10
-            }
-            ,
-            {
-                $project: {
-                    "_id": "$name"
+                    "_id": "False"
                 }
             }
         ]
     );
+    
             
-    retVal = retVal.map( function(retVal) { return retVal._id; });
-    
-    var temp = ["", "", "", "", "", "", "", "", "", ""];
-           
-    for (var i = 0; i < 10; ++i) {
-        temp[i] = retVal[i];
-    }
-    retVal = temp;
-    
-    return retVal;
-}
+    return retVal.map( function(retVal) { return retVal.rating; })[0];
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -353,63 +413,61 @@ function pitanje3 () {
     print ("Zakljucak: ");
     print ("Izdavac sa najviše DLC je: " + pabliserSaNajviseDlc + ".");
     print ("");
-}
-
-
-function pitanje4 () {
-    var prosek = getMostBangForBuckAvg ();
-    var srednja = getMostBangForBuckMed();
-    
-    print ("Pitanje 4");
-    print ("");
-    print ("Koje igre su najisplativije? (broj sati u igri/cena)");
-    print ("Upiti su vršeni po dva kiiterijuma. ");
-    print ("Prvi kriterijum je bio prosek sati u igri po ceni: " + prosek + ". ");
-    print ("Drugi kriterijum je bio srednja vrednost broja sati u igri po ceni: " + srednja + ". ");
-    print ("");
-    print ("Zakljucak: ");
-    print ("U zavisnosti od kriterijuma, najisplativijom igrom se moze smatrati " + prosek[0] + ", tj. " + srednja[0] + ". ");
-    print ("");
 }
 
 
+function pitanje4 () {
+    var prosek = getMostBangForBuckAvg ();
+    var srednja = getMostBangForBuckMed();
+    
+    print ("Pitanje 4");
+    print ("");
+    print ("Koje igre su najisplativije? (broj sati u igri/cena)");
+    print ("Upiti su vršeni po dva kiiterijuma. ");
+    print ("Prvi kriterijum je bio prosek sati u igri po ceni: " + prosek + ". ");
+    print ("Drugi kriterijum je bio srednja vrednost broja sati u igri po ceni: " + srednja + ". ");
+    print ("");
+    print ("Zakljucak: ");
+    print ("U zavisnosti od kriterijuma, najisplativijom igrom se može smatrati " + prosek[0] + ", tj. " + srednja[0] + ". ");
+    print ("");
+}
+
+
+function pitanje5 () {
+    var besplatne = prosecnaOcenaBesplatnih();
+    var platne = prosecnaOcenaPlatnih();
+
+    print ("Pitanje 5");
+    print ("");
+    print ("Da li su besplatne igre bolje ocenjene od onih koje se naplacuju?");
+    print ("Prosecna ocena besplatnih igara: " + besplatne);
+    print ("Prosecna ocena igara koje je se placaju: " + platne);
+    print ("");
+    print ("Zakljucak: ");
+    print ("Igre koje su besplatne " + ((besplatne > platne) ? ("jesu") : ("nisu")) + " bolje ocenjene od onih koje se placaju. ");
+    print ("");
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Poziv ispisa 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-pitanje1();
+pitanje1();
 
 pitanje2();
 
 pitanje3();
-
-pitanje4();
 
+pitanje4();
+
+pitanje5();
 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Development 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-db.steam_aggregate.aggregate(
-        [
-            {
-                $project: {
-                    "name": "$name", 
-                    "is_free": "$is_free", 
-                    "diff": {$differen}
-                }
-            }
-        ]
-    );
-
-
-
 
 
 
